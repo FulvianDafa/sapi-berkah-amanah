936293646d429b49fd59146ccc7a7e259a048d4a
zidandwip <zidandwipermana01@gmail.com>
2025-04-27 16:33:35 +0700

    chore(be): test api

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -4,6 +4,9 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\HewanKurbanController;
+use App\Http\Controllers\AuthApiController;
+
+Route::post('login', [AuthApiController::class, 'login']);
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::apiResource('HewanKurban', HewanKurbanController::class);
